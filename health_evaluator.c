243b1ab7d7a1db600df163cf913d6bcf7b07652b
dianee <dianeprincess817@gmail.com>
2025-12-06 13:43:31 +0800

    Add files via upload

---
+++ health_evaluator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Constants
+#define max_test 10
+#define max_recommendation 10
+#define config_file "patient_data.csv"
+#define report_file "evaluation_report.txt"
